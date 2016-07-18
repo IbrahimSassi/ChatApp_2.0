c04d3b9d4192987278d1750a09496c9b54789b2d
IbrahimSassi <sassiibrahim12@gmail.com>
2016-07-18 20:29:31 +0100

    'FinalizingLoginWithFacebook'

--- www/js/app.js
+++ www/js/app.js
@@ -11,50 +11,50 @@ angular.module('chatApp', [
     'ngCordovaOauth',
     'chatApp.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                // for form inputs)
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+                // Don't remove this line unless you know what you are doing. It stops the viewport
+                // from snapping when text inputs are focused. Ionic handles this internally for
+                // a much nicer keyboard experience.
+                cordova.plugins.Keyboard.disableScroll(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    })
 
 
-.config(function ($stateProvider,$urlRouterProvider) {
+    .config(function ($stateProvider, $urlRouterProvider) {
         $stateProvider
-            .state('login',{
-                url:'/login',
-                templateUrl:'templates/login.html',
-                controller:''
+            .state('login', {
+                url: '/login',
+                templateUrl: 'templates/login.html',
+                controller: ''
             })
-            .state('chat',{
-                url:'/chat/:nickname',
-                templateUrl:'templates/chat.html',
-                controller:''
+            .state('chat', {
+                url: '/chat/',
+                params: {data : null},
+                templateUrl: 'templates/chat.html',
+                controller: ''
             });
 
         $urlRouterProvider.otherwise('/login');
     })
 
 
+    .factory('Socket', function (socketFactory) {
+        var myIoSocket = io.connect('https://chatapp-ibrahimsassi.c9users.io');
 
-.factory('Socket', function (socketFactory) {
-    var myIoSocket = io.connect('https://chatapp-ibrahimsassi.c9users.io');
+        mySocket = socketFactory({
+            ioSocket: myIoSocket
+        });
 
-    mySocket = socketFactory({
-        ioSocket: myIoSocket
-    });
-
-    return mySocket;
-})
+        return mySocket;
+    })
 
--- www/js/controller.js
+++ www/js/controller.js
@@ -1,79 +1,60 @@
 angular.module('chatApp.controllers', [])
 
 
-    .controller('LoginController',function ($scope,
-                                            $state,
-                                            $cordovaOauth,
-                                            $http) {
+    .controller('LoginController', function ($scope,
+                                             $state,
+                                             $cordovaOauth,
+                                             $http) {
 
         $scope.join = function (nickname) {
 
-            if(nickname){
-                $state.go('chat',{
-                    nickname: nickname
-                });
+            if (nickname) {
+                $state.go('chat', {data : {
+                    nickname: nickname,
+                    displayPicture : 'https://mir-s3-cdn-cf.behance.net/project_modules/disp/a9475211889067.562541caf0859.png'
+                }});
             }
         }
 
 
         //LoginWithFacebook
 
-        $scope.user ={};
-        $scope.LoginWithFacebook = function() {
-            $cordovaOauth.facebook("CLient ID", ["email"]).then(function(result) {
+        $scope.user = {};
+        $scope.LoginWithFacebook = function () {
+            $cordovaOauth.facebook("App Id", ["email"]).then(function (result) {
                 // results
                 console.log(result);
-                $http.get('https://graph.facebook.com/v2.7/me?field=id,name,picture&access_token='+ result.access_token)
-                    .success(function (data,status,header,config) {
-                    $scope.user.fullName= data.name;
-                    $scope.user.displayPicture = data.picture.data.url;
-                    alert($scope.user.fullName + " " + $scope.user.displayPicture);
-
-                })
-            }, function(error) {
+                $http.get('https://graph.facebook.com/v2.7/me?field=id,name,picture&access_token=' + result.access_token)
+                    .success(function (data, status, header, config) {
+                        $scope.user.fullName = data.name;
+                        $scope.user.displayPicture = data.picture.data.url;
+                       // alert($scope.user.fullName + " " + $scope.user.displayPicture);
+                        $state.go('chat',{data :{nickname:$scope.user.fullName,
+                                        displayPicture:$scope.user.displayPicture}});
+                    })
+            }, function (error) {
                 console.log(error);
             });
         }
 
 
-
-
-
-
-
-
-
     })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    .controller('ChatController',function ($scope,$state,
-                                           $stateParams,Socket,
-                                           $ionicScrollDelegate,
-                                           $sce,
-                                           $timeout,
-                                           $cordovaMedia) {
+    .controller('ChatController', function ($scope, $state,
+                                            $stateParams, Socket,
+                                            $ionicScrollDelegate,
+                                            $sce,
+                                            $timeout,
+                                            $cordovaMedia) {
 
 
         $scope.status_message = "Welcome To ChatApp";
-        $scope.nickname = $stateParams.nickname;
-        $scope.messages=[];
-        $scope.displayPicture = 'https://mir-s3-cdn-cf.behance.net/project_modules/disp/a9475211889067.562541caf0859.png';
+        $scope.nickname = $stateParams.data.nickname;
+        $scope.messages = [];
+        $scope.displayPicture = $stateParams.data.displayPicture;
 
-        var COLORS = ['#f44336','#E91E63','#9C27B0','#3F51B5','#009688'];
+        var COLORS = ['#f44336', '#E91E63', '#9C27B0', '#3F51B5', '#009688'];
 
 
         Socket.on("connect", function () {
@@ -81,28 +62,26 @@ angular.module('chatApp.controllers', [])
 
             var data = {
                 message: $scope.nickname + " has Joined the chat !",
-                sender :$scope.nickname,
-                socketId :$scope.socketId,
-                isLog:true,
-                displayPicture :'',
-                color : $scope.getUsernameColor($scope.nickname)
+                sender: $scope.nickname,
+                socketId: $scope.socketId,
+                isLog: true,
+                displayPicture: '',
+                color: $scope.getUsernameColor($scope.nickname)
             };
 
-            Socket.emit("Message",data);
+            Socket.emit("Message", data);
         });
-        
+
         Socket.on("MessageReceived", function (dataFromServer) {
 
             dataFromServer.message = fillWithEmoticons(dataFromServer.message);
             dataFromServer.message = $sce.trustAsHtml(dataFromServer.message);
 
             $scope.messages.push(dataFromServer);
-            if($scope.socketId == dataFromServer.socketId)
-            {
+            if ($scope.socketId == dataFromServer.socketId) {
                 playAudio("audio/outgoing.mp3");
             }
-            else
-            {
+            else {
                 playAudio("audio/outgoing.mp3");
             }
 
@@ -112,39 +91,37 @@ angular.module('chatApp.controllers', [])
         })
 
 
-
-
-
-
         var typing = false;
-        var TYPING_TIMER_LENGTH =3000;
-
+        var TYPING_TIMER_LENGTH = 3000;
 
 
         //Typing Fon
 
         $scope.updateTyping = function () {
-            if(!typing){
+            if (!typing) {
                 typing = true;
-                Socket.emit("typing", {socketId : $scope.socketId,
-                    sender: $scope.nickname});
+                Socket.emit("typing", {
+                    socketId: $scope.socketId,
+                    sender: $scope.nickname
+                });
 
             }
 
             var lastTypingTime = (new Date()).getTime();
 
             $timeout(function () {
-                var timeDiff = (new Date()).getTime()- lastTypingTime ;
+                var timeDiff = (new Date()).getTime() - lastTypingTime;
 
-                if(timeDiff >= TYPING_TIMER_LENGTH && typing){
-                    Socket.emit('StopTyping',{socketId:$scope.socketId,
-                                                sender:$scope.nickname});
-                    typing=false;
+                if (timeDiff >= TYPING_TIMER_LENGTH && typing) {
+                    Socket.emit('StopTyping', {
+                        socketId: $scope.socketId,
+                        sender: $scope.nickname
+                    });
+                    typing = false;
                 }
 
-            },TYPING_TIMER_LENGTH);
+            }, TYPING_TIMER_LENGTH);
         }
-
 
 
         Socket.on('StopTyping', function (data) {
@@ -152,36 +129,27 @@ angular.module('chatApp.controllers', [])
         });
 
         Socket.on('typing', function (data) {
-            $scope.status_message= data.sender + ' is typing';
+            $scope.status_message = data.sender + ' is typing';
         });
 
 
+        function playAudio(src) {
 
-
-
-        function playAudio(src){
-
-            if(ionic.Platform.isAndroid() || ionic.Platform.isIOS())
-            {
+            if (ionic.Platform.isAndroid() || ionic.Platform.isIOS()) {
                 var newUrl = '';
-                if (ionic.Platform.isAndroid())
-                {
-                    newUrl = '/android_asset/www/'+src;
+                if (ionic.Platform.isAndroid()) {
+                    newUrl = '/android_asset/www/' + src;
                 }
-                else
-                {
-                    newUrl= src;
+                else {
+                    newUrl = src;
                 }
 
                 var media = $cordovaMedia.newMedia(newUrl);
                 media.play();
             }
-            else
-            {
+            else {
                 new Audio(src).play();
             }
-
-
 
 
         }
@@ -189,17 +157,17 @@ angular.module('chatApp.controllers', [])
 
         $scope.sendMessage = function () {
             var newMessage = {
-              sender :  '',
-                message :'',
-                socketId : '',
+                sender: '',
+                message: '',
+                socketId: '',
                 isLog: false,
-                displayPicture:'',
-                color : ''
+                displayPicture: '',
+                color: ''
             };
 
-            newMessage.sender= $scope.nickname;
+            newMessage.sender = $scope.nickname;
             newMessage.message = $scope.message;
-            newMessage.socketId =$scope.socketId;
+            newMessage.socketId = $scope.socketId;
             newMessage.isLog = false;
             newMessage.displayPicture = $scope.displayPicture;
             newMessage.color = $scope.getUsernameColor($scope.nickname);
@@ -209,29 +177,28 @@ angular.module('chatApp.controllers', [])
         }
 
 
-         function fillWithEmoticons(message){
-             //:p
-             message = message.replace(/\(y\)/g, "<img src='../img/emoticons/32 (2).png' width='25px' height='25px' />");
-             message = message.replace(/\:p/g, "<img src='../img/emoticons/32 (13).png' width='25px' height='25px' />");
-             message = message.replace(/\;\)/g, "<img src='../img/emoticons/32 (17).png' width='25px' height='25px' />");
-             message = message.replace(/\:D/g, "<img src='../img/emoticons/32 (20).png' width='25px' height='25px' />");
-             message = message.replace(/\:\)/g, "<img src='../img/emoticons/32 (36).png' width='25px' height='25px' />");
-             message = message.replace(/\:\(/g, "<img src='../img/emoticons/32 (11).png' width='25px' height='25px' />");
-             message = message.replace(/\♥/g, "<img src='../img/emoticons/32 (37).png' width='25px' height='25px' />");
-             return message;
-         }
-
-
-        $scope.getUsernameColor = function(username){
-            var hash = 7;
-
-            for(var i = 0; i<username.length;i++){
-                hash = username.charCodeAt(i) + (hash<<5) - hash;
-            }
-            var index = Math.abs(hash%COLORS.length);
-            return COLORS[index]
+        function fillWithEmoticons(message) {
+            //:p
+            message = message.replace(/\(y\)/g, "<img src='../img/emoticons/32 (2).png' width='25px' height='25px' />");
+            message = message.replace(/\:p/g, "<img src='../img/emoticons/32 (13).png' width='25px' height='25px' />");
+            message = message.replace(/\;\)/g, "<img src='../img/emoticons/32 (17).png' width='25px' height='25px' />");
+            message = message.replace(/\:D/g, "<img src='../img/emoticons/32 (20).png' width='25px' height='25px' />");
+            message = message.replace(/\:\)/g, "<img src='../img/emoticons/32 (36).png' width='25px' height='25px' />");
+            message = message.replace(/\:\(/g, "<img src='../img/emoticons/32 (11).png' width='25px' height='25px' />");
+            message = message.replace(/\♥/g, "<img src='../img/emoticons/32 (37).png' width='25px' height='25px' />");
+            return message;
         }
 
+
+        $scope.getUsernameColor = function (username) {
+            var hash = 7;
+
+            for (var i = 0; i < username.length; i++) {
+                hash = username.charCodeAt(i) + (hash << 5) - hash;
+            }
+            var index = Math.abs(hash % COLORS.length);
+            return COLORS[index]
+        }
 
 
     })
